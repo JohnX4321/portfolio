509e31d6d41b5416881e70d076bf91ff1a48c768
Samarth MS <johnx4321gaming@gmail.com>
2020-05-19 15:26:51 +0530

    Updates

---
+++ precache-manifest.6d1876739f3ee5a7ec998c79192d67b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67c0970f1f05da28ae92662066d32e4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1a6221929009384b2b43",
+    "url": "/portfolio/static/css/main.7e52178e.chunk.css"
+  },
+  {
+    "revision": "45f43663cf2532c3d62e",
+    "url": "/portfolio/static/js/2.2213c9df.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/portfolio/static/js/2.2213c9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a6221929009384b2b43",
+    "url": "/portfolio/static/js/main.9bd4999e.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  }
+]);
